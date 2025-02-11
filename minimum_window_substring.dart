f8e307d619bd425a453edb6cf444d97d9884541b
Vivek <vivektalreja88@gmail.com>
2025-02-11 15:19:00 +0500

    leetcode problem 76 done

--- minimum_window_substring.dart
+++ minimum_window_substring.dart
@@ -1,104 +1,13 @@
-// class Solution {
-//   String minWindow(String s, String t) {
-//     Map<String, int> originalMap = {};
-
-//     for (int i = 0; i < t.length; i++) {
-//       originalMap[t[i]] =
-//           originalMap[t[i]] != null ? originalMap[t[i]]! + 1 : 1;
-//     }
-//     print("originalMap: ${originalMap}");
-
-//     int start = 0,
-//         end = 0,
-//         n = t.length,
-//         beginPointer = 0,
-//         endPointer = 0,
-//         count = 0x7FFFFFFFFFFFFFFF;
-
-//     while (start < s.length && !t.contains(s[start])) {
-//       start++;
-//     }
-
-//     for (int i = 0; i < s.length; i++) {
-//       if (originalMap.containsKey(s[i]) && originalMap[s[i]]! > 0) {
-//         originalMap[s[i]] = originalMap[s[i]]! - 1;
-//         n--;
-//         end++;
-//         print("1");
-//         print("start: $start");
-//         print("end: $end");
-//         print("original: $originalMap");
-//       } else {
-//         if (t.contains(s[i]) &&
-//             originalMap[s[i]] != null &&
-//             originalMap[s[i]] == 0) {
-//           while ((originalMap[s[start]] == null ||
-//                   originalMap[s[start]]! >= 0 ||
-//                   originalMap[s[i]] == originalMap[s[start]]) &&
-//               (start < end)) {
-//             if (originalMap[s[start]] != null) {
-//               originalMap[s[start]] = originalMap[s[start]]! + 1;
-//               n = n + 1;
-//             }
-//             start++;
-//             print("2");
-//           }
-//           if (originalMap[s[i]]! > 0) {
-//             originalMap[s[i]] = originalMap[s[i]]! - 1;
-//             n = n - 1;
-//           }
-//           end++;
-//         } else {
-//           end++;
-//         }
-//         print("start: $start");
-//         print("end: $end");
-//       }
-//       if (n == 0) {
-//         if (end - start < count) {
-//           count = end - start;
-//           beginPointer = start;
-//           endPointer = end;
-//         }
-
-//         originalMap[s[start]] = originalMap[s[start]]! + 1;
-//         n = n + 1;
-//         start++;
-
-//         while (start < end && (!t.contains(s[start]))) {
-//           start++;
-//         }
-
-//         // if (originalMap.containsKey(s[i])) {
-//         //   originalMap[s[i]] = originalMap[s[i]]! - 1;
-//         //   n--;
-//         // }
-//         //end++;
-//       }
-//     }
-//     // print("begin pointer: $beginPointer");
-//     // print("end pointer: $endPointer");
-//     // print("string: ${s.substring(
-//     //   beginPointer,
-//     //   endPointer,
-//     // )}");
-//     // print("count: ${count < 0x7FFFFFFFFFFFFFFF ? count : 0}");
-
-//     return s.substring(
-//       beginPointer,
-//       endPointer,
-//     );
-//   }
-// }
 class Solution {
   String minWindow(String s, String t) {
     Map<String, int> originalMap = {};
 
-    for (int i = 0; i < t.length; i++) {
-      originalMap[t[i]] =
-          originalMap[t[i]] != null ? originalMap[t[i]]! + 1 : 1;
+    for (int i = 0; i < 128; i++) {
+      originalMap[String.fromCharCode(i)] = 0;
     }
-    print("originalMap: ${originalMap}");
+    for (int i = 0; i < t.length; i++) {
+      originalMap[t[i]] = originalMap[t[i]] != 0 ? originalMap[t[i]]! + 1 : 1;
+    }
 
     int start = 0,
         end = 0,
@@ -108,10 +17,10 @@ class Solution {
         count = 0x7FFFFFFFFFFFFFFF;
 
     while (end < s.length) {
-      if (originalMap[s[end]] != null && originalMap[s[end]]! > 0) {
-        originalMap[s[end]] = originalMap[s[end]]! - 1;
+      if (originalMap[s[end]]! > 0) {
         n--;
       }
+      originalMap[s[end]] = originalMap[s[end]]! - 1;
       end++;
       while (n == 0) {
         if (end - start < count) {
@@ -119,15 +28,12 @@ class Solution {
           beginPointer = start;
           endPointer = end;
         }
-        if (originalMap[s[start]] != null && originalMap[s[start]] == 0) {
-          print("start: $start");
-          originalMap[s[start]] = originalMap[s[start]]! + 1;
+        originalMap[s[start]] = originalMap[s[start]]! + 1;
+
+        if (originalMap[s[start]]! > 0) {
           n++;
         }
         start++;
-        // if (originalMap[s[start]] == null && start <= end) {
-        //   start++;
-        // }
       }
     }
 
